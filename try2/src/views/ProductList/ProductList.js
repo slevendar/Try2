52d40ca9fdb2572415d7b37205d57f7670fde716
Dor Avrahami <Dor@gkalk.com>
2020-05-24 17:59:05 +0300

    first

---
+++ try2/src/views/ProductList/ProductList.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/styles';
+import { IconButton, Grid, Typography } from '@material-ui/core';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import Button from '@material-ui/core/Button';
+import { ProductsToolbar, ProductCard } from './components';
+import mockData from './data';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    padding: theme.spacing(3)
+  },
+  content: {
+    marginTop: theme.spacing(2)
+  },
+  pagination: {
+    marginTop: theme.spacing(3),
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end'
+  }
+}));
+
+const ProductList = () => {
+  const classes = useStyles();
+  const [products] = useState(mockData);
+
+  return (
+    <div className={classes.root}>
+      <Button variant="contained" color="primary">
+        Submit new Barrel
+      </Button>
+    </div>
+  );
+};
+
+export default ProductList;
